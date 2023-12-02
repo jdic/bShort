428409dec425ffa236de04c050864ee4307b5f40
JMota <jmotadv@gmail.com>
2023-12-01 19:47:55 -0700

    ‎

--- utils/selectors.js
+++ utils/selectors.js
@@ -1,16 +1,6 @@
 const TITLE_SELECTOR = 'head > title'
-const FAVICON_OP1_SELECTOR = 'head > link[rel="icon"]'
-const FAVICON_OP2_SELECTOR = 'head > link[rel="shortcut icon"]'
-
-const TITLE_REGEX = /<title>(.*?)<\/title>/i
-const FAVICON_REGEX = /<link rel="(shortcut icon|icon)" href="(.*?)"\/>/i
 
 module.exports =
 {
-  TITLE_SELECTOR,
-  FAVICON_OP1_SELECTOR,
-  FAVICON_OP2_SELECTOR,
-
-  TITLE_REGEX, 
-  FAVICON_REGEX
+  TITLE_SELECTOR
 }
